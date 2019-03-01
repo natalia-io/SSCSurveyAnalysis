efa9b29ee84bc0b7d4ba3e3ab404d97c9ef2fe14
Natália Mendonça <44880543+natalia-io@users.noreply.github.com>
2019-02-28 20:27:04 -0800

    Update wellbeingmatrix.R

--- wellbeingmatrix.R
+++ wellbeingmatrix.R
@@ -1,44 +1,21 @@
-#This R script creates a correlation matrix between the well-being/happiness related variables in the 2019 SlateStarCodex survey and saves it in a .png image. 
-
-
-
 #First of all, clearing the existing environment
 
 rm(list=ls());
 
+#Unloading currently loaded packages
+lapply(paste('package:',names(sessionInfo()$otherPkgs),sep=""),detach,character.only=TRUE,unload=TRUE)
+
+
 #Defining the requred packages, installing the unavailable ones, and loading them into the environment
 
-requiredPackages <- c("ggplot2","openxlsx","stringr","gridExtra","knitr","dplyr","EnvStats","stats","ggpubr","grid","gtable","reshape2","GGally");
+requiredPackages <- c("ggplot2","openxlsx","stringr","gridExtra","knitr","dplyr","ggpubr","grid","gtable","reshape2","PerformanceAnalytics");
 unavailablePackages <- requiredPackages[!(requiredPackages %in% installed.packages()[,"Package"])];
 if(length(unavailablePackages)) install.packages(unavailablePackages);
 lapply(requiredPackages, library, character.only = TRUE);
 
-#Downloading the Excel spreadsheet from the SSC website
-
-ssc2019 <- read.xlsx("https://slatestarcodex.com/Stuff/ssc2019_public.xlsx");
-
-#Making sure column names are unique
-
-colnames(ssc2019) <- make.unique(names(ssc2019));
-
-#Filtering null values
-ssc2019 <- ssc2019 %>% 
-  filter(Mood.Scale != "NA")  %>%
-  filter(Life.Satisfaction != "NA")  %>%
-  filter(Anxiety.1 != "NA")  %>%
-  filter(Ambition != "NA")  %>%
-  filter(Status != "NA")  %>%
-  filter(Financial.Situation != "NA")  %>%
-  filter(Social.Skills != "NA")  %>%
-  filter(Romantic.Life != "NA");
-  
-#Reverse-coding the anxiety variables, so that higher values indicate lower anxiety
-ssc2019$Anxiety.Reversed <- 11 - ssc2019$Anxiety.1;
-
-#Defining the plot theme
 plottheme <- theme(
   plot.background = element_rect(fill = "#ffffff"),
-  plot.title = element_text(family = "Charter", color = "#333333", size = (20), face = "bold"),
+  plot.title = element_text(family = "Charter", color = "#333333", size = (40), face = "bold"),
   plot.subtitle = element_blank(),
   plot.caption = element_blank(),
   panel.background = element_rect(fill = alpha("#f5f5f5", 1)),
@@ -55,15 +32,28 @@ plottheme <- theme(
   axis.ticks.y = element_blank(),
   axis.text.x = element_blank(),
   axis.text.y = element_blank(),
-  axis.title = element_text(family = "Charter", color = "#333333", size = (20), face = "bold"),
+  axis.title = element_text(family = "Charter", color = "#333333", size = (40), face = "bold"),
 );
+
+ssc2019 <- read.xlsx("https://slatestarcodex.com/Stuff/ssc2019_public.xlsx");
+colnames(ssc2019) <- make.unique(names(ssc2019));
+ssc2019 <- ssc2019 %>% 
+  filter(Mood.Scale != "NA")  %>%
+  filter(Life.Satisfaction != "NA")  %>%
+  filter(Anxiety.1 != "NA")  %>%
+  filter(Ambition != "NA")  %>%
+  filter(Status != "NA")  %>%
+  filter(Financial.Situation != "NA")  %>%
+  filter(Social.Skills != "NA")  %>%
+  filter(Romantic.Life != "NA");
   
-#Subsetting the survey spreadsheet to only the desired variables
+#Reverse-coding the anxiety variables, so that higher values indicate lower anxiety
+ssc2019$Anxiety.Reversed <- 11 - ssc2019$Anxiety.1
+  
 subset <- select(ssc2019, Mood.Scale, Life.Satisfaction, Anxiety.Reversed, Ambition, Status, Social.Skills, Financial.Situation, Romantic.Life);
 
 length <- length(subset);
 
-#Making a list with all of the plots to be graphed
 plots <- list();
 
 for (i in 0:(length*length - 1))
@@ -71,26 +61,25 @@ for (i in 0:(length*length - 1))
  
  i <- i;
  row <- floor(i/length);
- xValue <- row + 1;
  
  if (i %% (length + 1) == 0) {
-    plots[[i + 1]] <<- ggplot(subset, aes(subset[[xValue]])) +
+    plots[[i + 1]] <<- ggplot(subset, aes(subset[[row + 1]])) +
                         geom_bar() +
                         plottheme +
                         scale_x_continuous(breaks = round(seq(min(0), max(10), by = 1),1)) +
                         labs(y="",x="",title="");
    } else {
     if (i %% length <= row) {
-      plots[[i + 1]] <<- ggplot(subset, aes(x=subset[[xValue]], y=subset[[i %% length + 1]])) +
+      plots[[i + 1]] <<- ggplot(subset, aes(x=subset[[row + 1]], y=subset[[i %% length + 1]])) +
                           geom_density2d() +
                           plottheme +
                           scale_x_continuous(breaks = round(seq(min(0), max(10), by = 1),1)) +
                           scale_y_continuous(breaks = round(seq(min(0), max(10), by = 1),1)) +
                           labs(y="",x="",title="");
     } else {
-      plots[[i + 1]] <<- ggplot(subset, aes(x = subset[[xValue]], y = subset[[i %% length + 1]])) + 
-                          geom_jitter(alpha=0.01, size=5, color="black") +
-                          stat_cor(method = "pearson", color="black", size=5, family="Charter", fontface="bold") +
+      correlation <- cor.test(subset[[row + 1]], subset[[i %% length + 1]], method = "pearson", conf.level = 0.95);
+      plots[[i + 1]] <<- ggplot(subset, aes(x = subset[[row + 1]], y = subset[[i %% length + 1]])) + 
+                          geom_text(aes(x=1,y=2000), label = paste("R = ",round(correlation$estimate, digits = 2),"\n p = ", correlation$p.value), size=15, family="Charter",fontface="italic") +
                           plottheme +
                           scale_x_continuous(breaks = round(seq(min(0), max(10), by = 1),1)) +
                           scale_y_continuous(breaks = round(seq(min(0), max(10), by = 1),1)) +
@@ -101,13 +90,11 @@ for (i in 0:(length*length - 1))
     plots[[i+1]] <<- plots[[i+1]] + labs(title=names(subset[i%%length+1]));
    }
    if(i %% length == 0) {
-    plots[[i+1]] <<- plots[[i+1]] + labs(y=names(subset[xValue]));
+    plots[[i+1]] <<- plots[[i+1]] + labs(y=paste("\n",names(subset[row + 1]),"\n"));
    }
 })
 
-#Finally, graphing the plots in a grid and saving the result to png-files/wellbeingmatrix.png.
-
-png(file="png-files/wellbeingmatrix.png", width = 3200, height = 3200, type = "quartz", res = 150);
+png(file="png-files/wellbeingmatrix.png", width = 7000, height = 7000, type = "quartz", res = 150);
 
 grid <- grid.arrange(
   grobs = plots, 
@@ -115,10 +102,10 @@ grid <- grid.arrange(
   nrow=length, 
   top = grid.text(
     "\n Well-being variables in the SSC survey \n", 
-    gp = gpar(fontsize = 37, col = "#333333", fontfamily="Charter")), 
+    gp = gpar(fontsize = 80, col = "#333333", fontfamily="Charter")), 
   bottom = grid.text(
     "\n Source: SlateStarCodex 2019 Survey \n", 
-    gp=gpar(fontsize=20, col="#111111",fontfamily="Charter",fontface="italic")
+    gp=gpar(fontsize=40, col="#111111",fontfamily="Charter",fontface="italic")
   )
 );
 
